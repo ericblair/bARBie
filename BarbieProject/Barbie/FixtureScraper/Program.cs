dcc1de923593f24bffe96df8aace63f976ed3202
ericblair <eric.t.blair@gmail.com>
2014-02-25 21:17:00 +0000

    Skeleton forming of new system. Got a scraper and updated entity model.

---
+++ BarbieProject/Barbie/FixtureScraper/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace FixtureScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = false
+            };
+
+            var process = new Process { StartInfo = startInfo };
+
+            process.Start();
+            //process.StandardInput.WriteLine(@"cd C:\bARBie\bARBie\ScrapingScripts\OddsChecker");
+            //process.StandardInput.WriteLine(@"node ocFootball.js >> log.txt");
+            //process.StandardInput.WriteLine("exit");
+            //process.WaitForExit();
+
+            process.StandardInput.WriteLine(@"cd C:\bARBie\bARBie\ScrapingScripts");
+            process.StandardInput.WriteLine(@"node paramTest.js one two three >> paramTest.txt");
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ BarbieProject/Barbie/Scrapers/Football/OCFixturesScraper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace Scrapers
+{
+    public class OCFixturesScraper
+    {
+        bARBieEntities barbieEntity = new bARBieEntities();
+        // Public methods:
+        // Scrape all fixtures
+        public void ScrapeAllFixtures()
+        {
+            // Get all competition urls from database
+            var competitions = barbieEntity.OddsCheckerCompetitionUrls;
+
+            foreach (var competition in competitions)
+            {
+                CallFixtureScraper(competition.CountryID, competition.CompetitionID, competition.Url);
+            }
+
+            // foreach competition url,
+            // call node to run the oc script, passing in url to scrape fixtures from
+        }
+        // Scrape all fixtures for country
+        // Scrape all fixtures for competetion
+
+        private void CallFixtureScraper(int? countryId, int competitionId, string competitionUrl)
+        {
+            // Thoughts: 
+            // By simply omitting the following lines:
+            //process.StandardInput.WriteLine("exit");
+            //process.WaitForExit();
+            // from the end of this method, it becomes asynch in the sense that the method does
+            // not wait for the node process to complete before returning.
+
+            // TODO: Load location of scraper script, name of file and log file from config file
+            var scraperFileHomeDir = @"C:\bARBie\bARBie\ScrapingScripts\OddsChecker";
+            var scraperFileName = "ocScrapeFootballFixtures.js";
+            var logFileName = "ocScrapeFootballFixtures.log";
+            
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            var process = new Process { StartInfo = startInfo };
+
+            process.Start();
+            process.StandardInput.WriteLine(@"cd " + scraperFileHomeDir);
+            process.StandardInput.WriteLine(@"node {0} {1] {2} {3} >> {4}", scraperFileName, 
+                                            countryId.HasValue ? countryId.Value.ToString() : "NULL", 
+                                            competitionId, competitionUrl, logFileName);
+            
+
+
+        }
+
+    }
+}

9c010e10068df4ade136a9f5cc73563dcf29e991
ericblair <eric.t.blair@gmail.com>
2014-02-28 23:37:14 +0000

    Refactored BuildScraperCommandPromptString

--- BarbieProject/Barbie/Scrapers/Football/OCMatchWinnerOddsScraper.cs
+++ BarbieProject/Barbie/Scrapers/Football/OCMatchWinnerOddsScraper.cs
@@ -14,6 +14,10 @@ namespace Scrapers.Football
     {
         bARBieEntities barbieEntity;
 
+        string scraperFileHomeDir = ConfigurationManager.AppSettings["OddsCheckerScriptsDir"];
+        string scraperFileName = ConfigurationManager.AppSettings["OddsCheckerMatchOddsScraperScript"];
+        string logFileName = ConfigurationManager.AppSettings["OddsCheckerMatchOddsScraperLog"];
+
         public OCMatchWinnerOddsScraper()
         {
             barbieEntity = new bARBieEntities();
@@ -21,70 +25,64 @@ namespace Scrapers.Football
 
         public void ScrapeAllOdds()
         {
-            var scraperFileHomeDir = ConfigurationManager.AppSettings["OddsCheckerScriptsDir"];
-            var scraperFileName = ConfigurationManager.AppSettings["OddsCheckerMatchOddsScraperScript"];
-            var logFileName = ConfigurationManager.AppSettings["OddsCheckerMatchOddsScraperLog"];
-
             var fixtures = barbieEntity.OddsCheckerFootballFixtures;
 
             var processCommands = new List<string>();
 
             foreach (var fixture in fixtures)
             {
-                var cmd = BuildScraperCommandPromptString
-                                (
-                                    scraperFileName, logFileName, fixture.ID,
-                                    fixture.CountryID, fixture.CompetitionID, fixture.HomeTeam,
-                                    fixture.AwayTeam, fixture.MatchWinnerOddsUrl
-                                );
+                var cmd = BuildScraperCommandPromptString(scraperFileName, logFileName, fixture);
 
                 processCommands.Add(cmd);
             }
 
+            CallNodeScripts(processCommands);
+        }
+
+        private void CallNodeScripts(List<string> processCommands)
+        {
             var allProcesses = Task.Factory.StartNew(() =>
+            {
+                var processes = processCommands.Select(cmd =>
                 {
-                    var processes = processCommands.Select(cmd =>
-                        {
-                            ProcessStartInfo startInfo = new ProcessStartInfo
-                            {
-                                FileName = "cmd.exe",
-                                RedirectStandardInput = true,
-                                RedirectStandardOutput = true,
-                                UseShellExecute = false,
-                                CreateNoWindow = true
-                            };
-
-                            var process = new Process { StartInfo = startInfo };
-
-                            process.Start();
-                            process.StandardInput.WriteLine(@"cd " + scraperFileHomeDir);
-                            process.StandardInput.WriteLine(cmd);
-                            process.StandardInput.WriteLine("exit");
-                            process.WaitForExit();
-
-                            return process;
-                        }).ToArray();
-
-                    foreach (var process in processes)
+                    ProcessStartInfo startInfo = new ProcessStartInfo
                     {
-                        process.WaitForExit();
-                        process.Dispose();
-                    }
-                });
+                        FileName = "cmd.exe",
+                        RedirectStandardInput = true,
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    };
+
+                    var process = new Process { StartInfo = startInfo };
+
+                    process.Start();
+                    process.StandardInput.WriteLine(@"cd " + scraperFileHomeDir);
+                    process.StandardInput.WriteLine(cmd);
+                    process.StandardInput.WriteLine("exit");
+                    process.WaitForExit();
+
+                    return process;
+                }).ToArray();
+
+                foreach (var process in processes)
+                {
+                    process.WaitForExit();
+                    process.Dispose();
+                }
+            });
 
             allProcesses.Wait();
-            
         }
 
         private string BuildScraperCommandPromptString(string scraperFileName, string logFileName, 
-                                                        int fixtureId, int? countryId, int competitionId,
-                                                        string homeTeam, string awayTeam, string matchWinnerOddsUrl)
+                                                        OddsCheckerFootballFixtures fixture)
         {
-            string countryIdString = countryId.HasValue ? countryId.Value.ToString() : "NULL";
+            string countryIdString = fixture.CountryID.HasValue ? fixture.CountryID.Value.ToString() : "NULL";
 
-            var nodeInputString = "node " + scraperFileName + " " + fixtureId 
-                                    + " " + countryIdString + " " + competitionId + " " + homeTeam 
-                                    + " " + awayTeam + " " + matchWinnerOddsUrl + " >> " + logFileName;
+            var nodeInputString = "node " + scraperFileName + " " + fixture.ID
+                                    + " " + countryIdString + " " + fixture.CompetitionID + " " + fixture.HomeTeam
+                                    + " " + fixture.AwayTeam + " " + fixture.MatchWinnerOddsUrl + " >> " + logFileName;
 
             return nodeInputString;
         }

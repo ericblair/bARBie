d5fed86f0637631ffb2188feb088cab9b3814ace
ericblair <eric.t.blair@gmail.com>
2014-03-04 23:26:58 +0000

    Cleaned up redundant code
    Removed out-dated and unnecessary entity model

--- BarbieProject/Barbie/Barbie/BetFairMatchOdds.cs
+++ BarbieProject/Barbie/Barbie/BetFairMatchOdds.cs
@@ -10,64 +10,64 @@ namespace Barbie
 {
     public class BetFairMatchOdds
     {
-        private BetFairFootballOdd _homeWinData = null;
-        private BetFairFootballOdd _awayWinData = null;
-        private BetFairFootballOdd _drawData = null;
+        //private BetFairFootballOdd _homeWinData = null;
+        //private BetFairFootballOdd _awayWinData = null;
+        //private BetFairFootballOdd _drawData = null;
 
-        public BetFairFootballOdd HomeWinData { get { return _homeWinData; } }
-        public BetFairFootballOdd AwayWinData { get { return _awayWinData; } }
-        public BetFairFootballOdd DrawData { get { return _drawData; } }
+        //public BetFairFootballOdd HomeWinData { get { return _homeWinData; } }
+        //public BetFairFootballOdd AwayWinData { get { return _awayWinData; } }
+        //public BetFairFootballOdd DrawData { get { return _drawData; } }
 
-        public BetFairMatchOdds(bARBieEntities barbieEntity, FootballMatch matchDetails)
-        {
-            SetHomeWinData(barbieEntity, matchDetails);
-            if (_homeWinData == null) return;
-            SetAwayWinData(barbieEntity, matchDetails);
-            if (_awayWinData == null) return;
-            SetDrawData(barbieEntity, matchDetails);
-        }
+        //public BetFairMatchOdds(bARBieEntities barbieEntity, FootballMatch matchDetails)
+        //{
+        //    SetHomeWinData(barbieEntity, matchDetails);
+        //    if (_homeWinData == null) return;
+        //    SetAwayWinData(barbieEntity, matchDetails);
+        //    if (_awayWinData == null) return;
+        //    SetDrawData(barbieEntity, matchDetails);
+        //}
 
-        public bool AreOddsSet { get { return oddsDataSet(); } }
+        //public bool AreOddsSet { get { return oddsDataSet(); } }
 
-        private bool oddsDataSet()
-        {
-            if (_homeWinData == null || _awayWinData == null || _drawData == null)
-                return false;
-            else
-                return true;
-        }
+        //private bool oddsDataSet()
+        //{
+        //    if (_homeWinData == null || _awayWinData == null || _drawData == null)
+        //        return false;
+        //    else
+        //        return true;
+        //}
 
-        private void SetHomeWinData(bARBieEntities barbieEntity, FootballMatch matchDetails)
-        {
-            _homeWinData = (from x in barbieEntity.BetFairFootballOdds
-                            where SqlFunctions.Difference(matchDetails.Fixture, x.Fixture) == 4
-                            && x.MatchDateTime > EntityFunctions.AddHours(matchDetails.MatchDateTime, -1)
-                            && x.MatchDateTime < EntityFunctions.AddHours(matchDetails.MatchDateTime, 3)
-                            && x.Prediction == x.HomeTeam
-                            orderby x.Updated descending
-                            select x).FirstOrDefault();
-        }
+        //private void SetHomeWinData(bARBieEntities barbieEntity, FootballMatch matchDetails)
+        //{
+        //    _homeWinData = (from x in barbieEntity.BetFairFootballOdds
+        //                    where SqlFunctions.Difference(matchDetails.Fixture, x.Fixture) == 4
+        //                    && x.MatchDateTime > EntityFunctions.AddHours(matchDetails.MatchDateTime, -1)
+        //                    && x.MatchDateTime < EntityFunctions.AddHours(matchDetails.MatchDateTime, 3)
+        //                    && x.Prediction == x.HomeTeam
+        //                    orderby x.Updated descending
+        //                    select x).FirstOrDefault();
+        //}
 
-        private void SetAwayWinData(bARBieEntities barbieEntity, FootballMatch matchDetails)
-        {
-            _awayWinData = (from x in barbieEntity.BetFairFootballOdds
-                            where SqlFunctions.Difference(matchDetails.Fixture, x.Fixture) == 4
-                            && x.MatchDateTime > EntityFunctions.AddHours(matchDetails.MatchDateTime, -1)
-                            && x.MatchDateTime < EntityFunctions.AddHours(matchDetails.MatchDateTime, 3)
-                            && x.Prediction == x.AwayTeam
-                            orderby x.Updated descending
-                            select x).FirstOrDefault();
-        }
+        //private void SetAwayWinData(bARBieEntities barbieEntity, FootballMatch matchDetails)
+        //{
+        //    _awayWinData = (from x in barbieEntity.BetFairFootballOdds
+        //                    where SqlFunctions.Difference(matchDetails.Fixture, x.Fixture) == 4
+        //                    && x.MatchDateTime > EntityFunctions.AddHours(matchDetails.MatchDateTime, -1)
+        //                    && x.MatchDateTime < EntityFunctions.AddHours(matchDetails.MatchDateTime, 3)
+        //                    && x.Prediction == x.AwayTeam
+        //                    orderby x.Updated descending
+        //                    select x).FirstOrDefault();
+        //}
 
-        private void SetDrawData(bARBieEntities barbieEntity, FootballMatch matchDetails)
-        {
-            _drawData = (from x in barbieEntity.BetFairFootballOdds
-                         where SqlFunctions.Difference(matchDetails.Fixture, x.Fixture) == 4
-                         && x.MatchDateTime > EntityFunctions.AddHours(matchDetails.MatchDateTime, -1)
-                         && x.MatchDateTime < EntityFunctions.AddHours(matchDetails.MatchDateTime, 3)
-                         && x.Prediction == "draw"
-                         orderby x.Updated descending
-                         select x).FirstOrDefault();
-        }
+        //private void SetDrawData(bARBieEntities barbieEntity, FootballMatch matchDetails)
+        //{
+        //    _drawData = (from x in barbieEntity.BetFairFootballOdds
+        //                 where SqlFunctions.Difference(matchDetails.Fixture, x.Fixture) == 4
+        //                 && x.MatchDateTime > EntityFunctions.AddHours(matchDetails.MatchDateTime, -1)
+        //                 && x.MatchDateTime < EntityFunctions.AddHours(matchDetails.MatchDateTime, 3)
+        //                 && x.Prediction == "draw"
+        //                 orderby x.Updated descending
+        //                 select x).FirstOrDefault();
+        //}
     }
 }
--- BarbieProject/Barbie/Barbie/OddsDataCollection.cs
+++ BarbieProject/Barbie/Barbie/OddsDataCollection.cs
@@ -3,66 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL;
 
 namespace Barbie
 {
     public class OddsCheckerMatchOdds
     {
-        private OddsCheckerFootballOdd _homeWinData = null;
-        private OddsCheckerFootballOdd _awayWinData = null;
-        private OddsCheckerFootballOdd _drawData = null;
+        //private OddsCheckerFootballOdds _homeWinData = null;
+        //private OddsCheckerFootballOdds _awayWinData = null;
+        //private OddsCheckerFootballOdds _drawData = null;
 
-        public OddsCheckerFootballOdd HomeWinData { get { return _homeWinData; } }
-        public OddsCheckerFootballOdd AwayWinData { get { return _awayWinData; } }
-        public OddsCheckerFootballOdd DrawData { get { return _drawData; } }
+        //public OddsCheckerFootballOdds HomeWinData { get { return _homeWinData; } }
+        //public OddsCheckerFootballOdds AwayWinData { get { return _awayWinData; } }
+        //public OddsCheckerFootballOdds DrawData { get { return _drawData; } }
 
-        public OddsCheckerMatchOdds(bARBieEntities barbieEntity, FootballMatch matchDetails)
-        {
-            SetHomeWinData(barbieEntity, matchDetails);
-            if (_homeWinData == null) return;
-            SetAwayWinData(barbieEntity, matchDetails);
-            if (_awayWinData == null) return;
-            SetDrawData(barbieEntity, matchDetails);
-        }
+        //public OddsCheckerMatchOdds(bARBieEntities barbieEntity, FootballMatch matchDetails)
+        //{
+        //    SetHomeWinData(barbieEntity, matchDetails);
+        //    if (_homeWinData == null) return;
+        //    SetAwayWinData(barbieEntity, matchDetails);
+        //    if (_awayWinData == null) return;
+        //    SetDrawData(barbieEntity, matchDetails);
+        //}
 
-        public bool AreOddsSet { get { return oddsDataSet(); } }
+        //public bool AreOddsSet { get { return oddsDataSet(); } }
 
-        private bool oddsDataSet()
-        {
-            if (_homeWinData == null || _awayWinData == null || _drawData == null)
-                return false;
-            else
-                return true;
-        }
+        //private bool oddsDataSet()
+        //{
+        //    if (_homeWinData == null || _awayWinData == null || _drawData == null)
+        //        return false;
+        //    else
+        //        return true;
+        //}
 
-        private void SetHomeWinData(bARBieEntities barbieEntity, FootballMatch matchDetails)
-        {
-            _homeWinData = (from x in barbieEntity.OddsCheckerFootballOdds
-                            where x.Fixture == matchDetails.Fixture
-                            && x.MatchDateTime == matchDetails.MatchDateTime
-                            && x.Prediction == x.HomeTeam
-                            orderby x.Updated descending
-                            select x).FirstOrDefault();
-        }
+        //private void SetHomeWinData(bARBieEntities barbieEntity, FootballMatch matchDetails)
+        //{
+        //    _homeWinData = (from x in barbieEntity.OddsCheckerFootballOdds
+        //                    where x.Fixture == matchDetails.Fixture
+        //                    && x.MatchDateTime == matchDetails.MatchDateTime
+        //                    && x.Prediction == x.HomeTeam
+        //                    orderby x.Updated descending
+        //                    select x).FirstOrDefault();
+        //}
 
-        private void SetAwayWinData(bARBieEntities barbieEntity, FootballMatch matchDetails)
-        {
-            _awayWinData = (from x in barbieEntity.OddsCheckerFootballOdds
-                            where x.Fixture == matchDetails.Fixture
-                            && x.MatchDateTime == matchDetails.MatchDateTime
-                            && x.Prediction == x.AwayTeam
-                            orderby x.Updated descending
-                            select x).FirstOrDefault();
-        }
+        //private void SetAwayWinData(bARBieEntities barbieEntity, FootballMatch matchDetails)
+        //{
+        //    _awayWinData = (from x in barbieEntity.OddsCheckerFootballOdds
+        //                    where x.Fixture == matchDetails.Fixture
+        //                    && x.MatchDateTime == matchDetails.MatchDateTime
+        //                    && x.Prediction == x.AwayTeam
+        //                    orderby x.Updated descending
+        //                    select x).FirstOrDefault();
+        //}
 
-        private void SetDrawData(bARBieEntities barbieEntity, FootballMatch matchDetails)
-        {
-            _drawData = (from x in barbieEntity.OddsCheckerFootballOdds
-                         where x.Fixture == matchDetails.Fixture
-                         && x.MatchDateTime == matchDetails.MatchDateTime
-                         && x.Prediction == "draw"
-                         orderby x.Updated descending
-                         select x).FirstOrDefault();
-        }
+        //private void SetDrawData(bARBieEntities barbieEntity, FootballMatch matchDetails)
+        //{
+        //    _drawData = (from x in barbieEntity.OddsCheckerFootballOdds
+        //                 where x.Fixture == matchDetails.Fixture
+        //                 && x.MatchDateTime == matchDetails.MatchDateTime
+        //                 && x.Prediction == "draw"
+        //                 orderby x.Updated descending
+        //                 select x).FirstOrDefault();
+        //}
     }
 }

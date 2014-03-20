a44b6a1283c4b737aafaf404101d924208097443
ericblair <eric.t.blair@gmail.com>
2014-03-20 00:36:46 +0000

    Updated database build
    Updated table scripts and the directory structure. Added a build script
    (untested).

---
+++ DBScripts/buildDB.bat
@@ -0,0 +1,13 @@
+cd Tables
+
+osql -E -i Arbs_Football_MatchWinner.sql
+osql -E -i BetFairFootballOdds.sql
+osql -E -i OddsCheckerFootballOdds.sql
+osql -E -i FootballFixturesMap.sql
+osql -E -i BetFairFootballFixtures.sql
+osql -E -i OddsCheckerFootballFixtures.sql
+osql -E -i BetFairCompetitionUrls.sql
+osql -E -i OddsCheckerCompetitionUrls.sql
+osql -E -i FootballCompetitions.sql
+osql -E -i Countries.sql
+osql -E -i Bookie.sql

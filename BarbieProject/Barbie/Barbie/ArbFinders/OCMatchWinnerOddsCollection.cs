c261545a57b9dbe961382703dd5b8eab8a6dd531
ericblair <eric.t.blair@gmail.com>
2014-04-02 19:59:05 +0100

    Added comment

--- BarbieProject/Barbie/Barbie/ArbFinders/OCMatchWinnerOddsCollection.cs
+++ BarbieProject/Barbie/Barbie/ArbFinders/OCMatchWinnerOddsCollection.cs
@@ -7,6 +7,9 @@ using DAL;
 
 namespace Barbie.ArbFinders
 {
+    /// <summary>
+    /// This class provides properties providing the latest oddschecker match winner odds for a fixture
+    /// </summary>
     public class OCMatchWinnerOddsCollection
     {
         bARBieEntities _barbieEntity;
